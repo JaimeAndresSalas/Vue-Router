f7ba13516aadbd5e31d41e736711dfa9e09c5eed
Jaime Andres Salas <jsalasmoreira@gmail.com>
2020-04-25 16:09:05 -0500

    Paginacion del Imagenes

--- src/views/Fotos.vue
+++ src/views/Fotos.vue
@@ -1,7 +1,15 @@
 <template>
   <div>
     <h1>Galeria de Fotos</h1>
+    <router-link :to="{name:'fotos', params:{id: item}}" v-for="(item,index) of fotos" :key="index">
+      <button class="btn btn-alert">Foto {{item}}</button>
+    </router-link>
+
     <Imagen/>
+
+    <button @click="home">Home</button>
+    <button @click="regresar">Regresar</button>
+    <button @click="siguiente">Siguiente</button>
   </div>
 </template>
 
@@ -11,6 +19,22 @@ import Imagen from "@/components/Imagen.vue";
 export default {
   components: {
     Imagen
+  },
+  data() {
+    return {
+      fotos: [1, 2, 3]
+    };
+  },
+  methods: {
+    home() {
+      this.$router.push("/");
+    },
+    regresar(){
+      this.$router.got(-1)
+    },
+    siguiente(){
+      this.$router.got(1)
+    }
   }
 };
 </script>
